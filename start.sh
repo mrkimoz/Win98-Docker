592ce92902aaa3f18aa7c592043045542fd21ed4
mrkimoz <75150244+mrkimoz@users.noreply.github.com>
2024-03-24 04:52:30 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+# By : Eng. Mohamed Kamal
+# Email : Mr.peacock2@gmail.com
 
 # Start QEMU with the Windows 98 disk image and VNC support
 qemu-system-x86_64 \
     -drive file=win98.img,format=raw \
-    -m 64 \
+    -m 128 \
     -device cirrus-vga \
     -net nic,model=rtl8139 \
     -net user \
